6ddccddd4357588ad8e08ceb06e6c7278fcbe5f3
hammad-Ibrahim <hammad.ibrahim21994@gmail.com>
2025-09-07 07:06:40 +0300

    small fix

--- backend_security/settings.py
+++ backend_security/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'iptracking.apps.IpTrackingConfig',
+    'ip_tracking.apps.IpTrackingConfig',
 ]
 
 MIDDLEWARE = [
